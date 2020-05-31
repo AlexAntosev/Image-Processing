a24c0b7122415ee378bee09421387d18a4afc13b
Alex Antosev <alexander.antosev@gmail.com>
2020-05-31 13:07:32 +0300

    filtering and transformation

---
+++ gray-level-transformation.py
@@ -0,0 +1,20 @@
+
+from PIL import Image
+from pylab import *
+
+START_INTERVAL = 100.0
+END_INTERVAL = 255.0
+
+NEGATION = 255.0
+
+image = array(Image.open('images/doctor.jpg').convert('L'))
+gray()
+
+negative = NEGATION - image
+clamped = (START_INTERVAL / NEGATION) * image + START_INTERVAL 
+
+transformed = END_INTERVAL * (image / END_INTERVAL) ** 2
+
+imshow(transformed)
+
+show()
